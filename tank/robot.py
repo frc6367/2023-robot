7388363b2fb4945e0eb615d84494e96dd341cb6a
FRC6367 Laptop <>
2023-01-21 15:39:52 -0500

    basic autonomous

--- tank/robot.py
+++ tank/robot.py
@@ -6,6 +6,7 @@
 """
 
 import ctre
+import navx
 import wpilib
 from wpilib.drive import DifferentialDrive
 
@@ -20,7 +21,12 @@ class MyRobot(wpilib.TimedRobot):
         self.frontRightMotor = ctre.WPI_TalonSRX(1)
         self.rearRightMotor = ctre.WPI_TalonSRX(2)
 
-         # invert the right side motors
+        # Timer for autonomys
+        self.timer = wpilib.Timer()
+
+        self.gyro = navx.AHRS.create_spi()
+
+        # invert the right side motors
         # you may need to change or remove this to match your robot
         self.frontRightMotor.setInverted(True)
         self.rearRightMotor.setInverted(True)
@@ -44,6 +50,35 @@ class MyRobot(wpilib.TimedRobot):
     def teleopPeriodic(self):
         """Runs the motors with tank steering"""
         self.myRobot.tankDrive(self.leftStick.getY() * -1, self.rightStick.getY() * -1)
+
+    # Autonomous
+    def autonomousInit(self) -> None:
+        self.timer.reset()
+        self.timer.start()
+        self.gyro.reset()
+
+    def autonomousPeriodic(self) -> None:
+        if self.timer.get() < 3.0:
+            self.myRobot.arcadeDrive(0.25, 0, False)
+        elif self.timer.get() < 5.0:
+            self.myRobot.arcadeDrive(0, 0, False)
+        elif self.timer.get() < 10.0:
+            a = self.gyro.getAngle()
+            v = 0.8 + (a / 180) * 0.8
+            self.myRobot.arcadeDrive(0, v, False)
+        elif self.timer.get() < 13.0:
+            self.myRobot.arcadeDrive(0.25, 0, False)
+        elif self.timer.get() < 15.0:
+            self.myRobot.arcadeDrive(0, 0, False)
+            self.gyro.reset()
+        elif self.timer.get() < 20.0:
+            a = self.gyro.getAngle()
+            v = 0.8 + (a / 180) * 0.8
+            self.myRobot.arcadeDrive(0, v, False)
+        elif self.timer.get() < 23.0:
+            self.myRobot.arcadeDrive(0.25, 0, False)
+        else:
+            self.myRobot.arcadeDrive(0, 0, False)
 
 
 if __name__ == "__main__":
