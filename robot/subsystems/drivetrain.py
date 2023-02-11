55ba8c2c2ad6ae8e9a041f7f2431b011e31ac717
FRC6367 Laptop <>
2023-02-11 14:42:13 -0500

    create drivetrain.py

---
+++ robot/subsystems/drivetrain.py
@@ -0,0 +1,19 @@
+import wpilib.drive
+import ctre
+import magicbot
+
+class DriveTrain:
+    frontLeftMotor:  ctre.WPI_TalonSRX
+    rearLeftMotor:  ctre.WPI_TalonSRX
+    frontRightMotor:  ctre.WPI_TalonSRX
+    rearRightMotor:  ctre.WPI_TalonSRX
+
+    speed = magicbot.will_reset_to(0)
+    rotation = magicbot.will_reset_to(0)
+
+    def setup(self):
+        self.drive = wpilib.drive.DifferentialDrive(self.frontLeftMotor, self.rearLeftMotor, self.frontRightMotor, self.rearRightMotor )
+    
+    def move(self, speed: float, rotation: float):
+        self.speed = speed
+        self.rotation = rotation
