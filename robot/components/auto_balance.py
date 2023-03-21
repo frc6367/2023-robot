d5a987eea40d90bfd23e8bcee588bf4eb8904c59
FRC6367 Laptop <>
2023-03-21 15:56:38 -0400

    putting back old qualites of auto-balance

--- robot/components/auto_balance.py
+++ robot/components/auto_balance.py
@@ -1,8 +1,23 @@
 import math
 import wpilib
+import magicbot
+
+import navx
+from wpimath.controller import PIDController
+
+from subsystems.drivetrain import DriveTrain
 
 
 class AutoBalance:
+    ahrs: navx.AHRS
+    kP = magicbot.tunable(0.1)
+    maxOutMaintain = magicbot.tunable(0.15)
+    maxOutOvercome = magicbot.tunable(0.31)
+    maxR = magicbot.tunable(0.2)
+    drivetrain: DriveTrain
+
+    active = magicbot.will_reset_to(False)
+
     def __init__(self):
         self.mRioAccel = wpilib.BuiltInAccelerometer()
 
@@ -40,6 +55,46 @@ class AutoBalance:
 
         # # Amount of time to drive forward to secure the scoring of the gamepiece
         # self.doubleTapTime = 0.3
+
+    def setup(self):
+        self.pid = PIDController(self.kP, 0, 0)
+        self.pid.setSetpoint(0)
+        self.was_active = False
+
+        turnController = PIDController(
+            0.03,
+            0,
+            0.0,
+        )
+        turnController.enableContinuousInput(-180.0, 180.0)
+        turnController.setTolerance(2)
+        self.turnController = turnController
+
+    def maintain(self):
+        self._activate(self.maxOutMaintain)
+
+    def overcome(self, n):
+        self.extra = n
+        self.maintain()
+
+    def _activate(self, maxOut):
+        self.active = True
+        self.maxOut = maxOut
+
+        if not self.was_active:
+            self.pid.reset()
+            self.pid.setP(self.kP)
+
+            self.turnController.reset()
+            self.turnController.setSetpoint(self.ahrs.getYaw())
+
+    @magicbot.feedback
+    def get_angle(self):
+        return self.ahrs.getRoll()
+
+    @magicbot.feedback
+    def get_yaw(self):
+        return self.ahrs.getYaw()
 
     def getPitch(self) -> float:
         return (
@@ -118,16 +173,38 @@ class AutoBalance:
 
         else:
             return 0
-        
+
     def execute(self):
-        if (
-            self.getPitch
-            and self.getRoll
-            and self.getTilt
-            and self.secondsToTicks
-        ):
+        if self.getPitch and self.getRoll and self.getTilt and self.secondsToTicks:
             self.autoBalanceRoutine
-   
+        if self.active:
+            angle = self.get_angle()
+
+            if angle > self.fwd_angle or angle < self.rev_angle:
+                if angle > self.fwd_angle:
+                    angle -= self.fwd_angle
+                # else:
+                #     angle -= self.rev_angle
+
+                speed = self.pid.calculate(angle)
+                maxOut = abs(self.maxOut)
+                if speed > maxOut:
+                    speed = maxOut
+                elif speed < -maxOut:
+                    speed = -maxOut
+
+                speed = speed + self.extra
+
+                # rotation = -self.turnController.calculate(self.ahrs.getYaw())
+                # maxR = abs(self.maxR)
+                # if rotation > maxR:
+                #     rotation = maxR
+                # elif rotation < -maxR:
+                #     rotation = -maxR
+
+                self.drivetrain.move(speed, 0)
+
+        self.was_active = self.active
 
 
 # import magicbot
@@ -144,10 +221,10 @@ class AutoBalance:
 
 #     active = magicbot.will_reset_to(False)
 
-#     kP = magicbot.tunable(0.1)
-#     maxOutMaintain = magicbot.tunable(0.15)
-#     maxOutOvercome = magicbot.tunable(0.31)
-#     maxR = magicbot.tunable(0.2)
+# kP = magicbot.tunable(0.1)
+# maxOutMaintain = magicbot.tunable(0.15)
+# maxOutOvercome = magicbot.tunable(0.31)
+# maxR = magicbot.tunable(0.2)
 
 #     # Note to self: +/- 5% should be ok for autobalance??
 #     fwd_angle = magicbot.tunable(3)
@@ -169,23 +246,23 @@ class AutoBalance:
 #         turnController.setTolerance(2)
 #         self.turnController = turnController
 
-#     def maintain(self):
-#         self._activate(self.maxOutMaintain)
+# def maintain(self):
+#     self._activate(self.maxOutMaintain)
 
-#     def overcome(self, n):
-#         self.extra = n
-#         self.maintain()
+# def overcome(self, n):
+#     self.extra = n
+#     self.maintain()
 
-#     def _activate(self, maxOut):
-#         self.active = True
-#         self.maxOut = maxOut
+# def _activate(self, maxOut):
+#     self.active = True
+#     self.maxOut = maxOut
 
-#         if not self.was_active:
-#             self.pid.reset()
-#             self.pid.setP(self.kP)
+#     if not self.was_active:
+#         self.pid.reset()
+#         self.pid.setP(self.kP)
 
-#             self.turnController.reset()
-#             self.turnController.setSetpoint(self.ahrs.getYaw())
+#         self.turnController.reset()
+#         self.turnController.setSetpoint(self.ahrs.getYaw())
 
 #     @magicbot.feedback
 #     def get_angle(self):
